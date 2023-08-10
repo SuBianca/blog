e7327d5a329086a0c35a8f78449eef2a41514645
sugako.tamamoto <e23009@std.it-college.ac.jp>
2023-08-10 21:10:08 +0900

    2.6 layout

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,8 @@
 import Hero from 'components/hero'
 
-export default function Home () {
-  return <Hero />
+const Home = () => {
+  return (
+    <Hero />
+  )
 }
-
-// const Home = () => {
-//   return (
-//     <Layout>
-//       <Hero />
-//      </Layout>
-//   )
-// }
-// export default Home
+export default Home
