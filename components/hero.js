d2437b83c80936462ebe60ab7df315e06825cc32
sugako.tamamoto <e23009@std.it-college.ac.jp>
2023-08-15 00:52:35 +0900

    chapter2finished

--- components/hero.js
+++ components/hero.js
@@ -1,10 +1,9 @@
-const Hero = () => {
-  return (
-    <div>
-      <h1>CUBE</h1>
-      <p>アウトプットしていくサイト</p>
-    </div>
-  )
-}
+const Hero = ({ title, subtitle, imageOn = false }) => (
+  <>
+    <h1>{title}</h1>
+    <p>{subtitle}</p>
+    {imageOn && <figure>[画像]</figure>}
+  </>
+)
 
 export default Hero
